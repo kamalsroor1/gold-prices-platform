135b34fcb1e9dd5ec198bef12993a5cb6bc66665
kamalsroor1 <teko.2031@gmail.com>
2026-05-29 17:20:24 +0300

    feat(api): add base controller for Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="Gold Prices Platform API",
+ *     version="1.0.0",
+ *     description="API Documentation for Gold Prices & Bullion Tracking Platform",
+ *     @OA\Contact(
+ *         email="support@goldprices.com"
+ *     )
+ * )
+ * @OA\Server(
+ *     url="http://localhost:8000/api/v1",
+ *     description="Local Development API Server"
+ * )
+ */
 abstract class Controller
 {
     //
