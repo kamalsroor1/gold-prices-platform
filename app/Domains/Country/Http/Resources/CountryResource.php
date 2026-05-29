49366e15c19cb168d5e48926a2ef0c4cb9a620c2
kamalsroor1 <teko.2031@gmail.com>
2026-05-29 17:20:21 +0300

    feat(api): add Swagger annotations for CountryResource

--- app/Domains/Country/Http/Resources/CountryResource.php
+++ app/Domains/Country/Http/Resources/CountryResource.php
@@ -5,6 +5,18 @@ namespace App\Domains\Country\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="CountryResource",
+ *     title="Country Resource",
+ *     description="Country resource representation",
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="name", type="string", example="Saudi Arabia"),
+ *     @OA\Property(property="code", type="string", example="SA"),
+ *     @OA\Property(property="currency", type="string", example="SAR"),
+ *     @OA\Property(property="flag_url", type="string", example="https://example.com/flags/sa.png")
+ * )
+ */
 class CountryResource extends JsonResource
 {
     /**
